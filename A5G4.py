23246a9465122e6c9087aafa46025c36bc1a644f
ispc-programador2022 <wacursando@gmail.com>
2022-06-27 23:00:19 -0300

    Copia del menu principal

--- A5G4.py
+++ A5G4.py
@@ -1,1 +1,40 @@
-#comenzar ejercicio
+#Menu principal
+from unittest import case
+
+
+menu = """
+1- función suma, retorna la suma de 2 parámetros.
+2- función resta, retorna la resta de 2 parámetros.
+3- función producto, retorna el producto de 2 parámetros.
+4- función cociente, retorna el cociente de 2 parámetros.
+5- función módulo, retorna el módulo de 2 parámetros.
+6- función potencia, retorna la potencia del primero elevado al segundo parámetros.
+7- función radicación, retorna la raiz del primero respecto del segundo parámetros.
+8- función p1, retorna el producto de los 2 primero más el 3er parámetros, usando las
+funciones anteriores.
+9- función p1, retorna la suma de los 2 primero por el 3er parámetros, usando las
+funciones anteriores.
+10- función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones
+anteriores
+11- función genrnd que retorna una lista con 50 números aleatorios.
+12- función que devuelva la suma de las combinaciones posibles de los números generados
+por la función genrnd tomados de a dos.
+13- función que devuelva el producto de las combinaciones posibles de los númerosgit 
+generados por la función genrnd tomados de a dos.
+14- función que devuelva el producto de las combinaciones posibles de los números
+generados por la función genrnd tomados de a dos.
+15- función que calcule la media del vector obtenido en genrnd.
+16- función que calcule la mediana del vector obtenido en genrnd.
+17- función que calcule el rango del vector obtenido en genrnd.
+18- función que calcule la varianza del vector obtenido en genrnd.
+19- función que calcule devuelva el mínimo del vector obtenido en genrnd.
+20- función que calcule devuelva el máximo del vector obtenido en genrnd.
+21- función genrnd que retorna una lista con 500.000.000.000.000.000 números aleatorios.
+22- medir el tiempo de ejecución del 16 al 21
+23- medir el tiempo de ejecución del 22 al 28"""
+print(menu)
+print("Elija el nro de la función a ejecutar ")
+
+opcion = input()
+
+
