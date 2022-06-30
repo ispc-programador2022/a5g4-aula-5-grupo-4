00169f9ed0b4e70c27a002082c7251fef6bc7712
rofernandez22 <rofxrnandez22@gmail.com>
2022-06-30 03:19:33 -0300

    actividad 3 terminada

---
+++ actividad3.py
@@ -0,0 +1,7 @@
+def funcion_producto(num1, num2):
+    return num1*num2
+
+num1 = int(input("Ingrese un primer número: "))
+num2 = int(input("Ingrese un segundo número: "))
+
+print ("El producto es: ", funcion_producto(num1,num2))
