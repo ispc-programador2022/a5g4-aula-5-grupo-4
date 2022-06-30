473a5d0368f47ea9fabe0c5b7f68a422f4801ef5
rofernandez22 <rofxrnandez22@gmail.com>
2022-06-30 03:41:28 -0300

    actividad 4 terminada

---
+++ actividad4.py
@@ -0,0 +1,10 @@
+print("Actividad 4")
+print("Función cociente: Retorna el cociente de 2 parámetros")
+
+def funcion_cociente(num1, num2):
+    return num1//num2
+
+num1 = int(input("Ingrese un primer número: "))
+num2 = int(input("Ingrese un segundo número: "))
+
+print ("El cociente es: ", funcion_cociente(num1,num2))
