f820a95fb43ca5769041e94d01f7fdba8133d5cc
rofernandez22 <rofxrnandez22@gmail.com>
2022-06-30 04:28:57 -0300

    actividad 9 CON ERROR

---
+++ actividad9.py
@@ -0,0 +1,11 @@
+print("Actividad 9")
+print("Función p1: Retorna el producto de los 2 primeros parámetros más un 3er parámetro")
+
+from actividad3 import funcion_producto
+
+num3 = int(input("Ingrese un tercer número: "))
+
+total = funcion_producto(num1,num2)*num3
+
+print ("El producto total es: ", total)
+
