f7ab76c89067fadd2639a6df6a78924cff3c63a6
rofernandez22 <rofxrnandez22@gmail.com>
2022-06-30 03:58:30 -0300

    actividad 5 terminada

---
+++ actividad5.py
@@ -0,0 +1,10 @@
+print("Actividad 5")
+print("Función módulo: Retorna el módulo de 2 parámetros")
+
+def funcion_modulo(num1, num2):
+    return num1%num2
+
+num1 = int(input("Ingrese un primer número: "))
+num2 = int(input("Ingrese un segundo número: "))
+
+print ("El módulo es: ", funcion_modulo(num1,num2))
